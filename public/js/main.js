20b2c12e261a9c2074b6db5bf214aa3507dd6f72
Emily <eh1691@nyu.edu>
2021-05-05 07:29:58 +0000

    jquery library is added before the script so the delete button works now by adding the cdn

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //client side javascript file
 //alert('Testing main.js')
 /*
@@ -6,11 +7,19 @@ To make a delete request, you cannot just use a link or submitting a form. You h
 Use jquery to make a delete request with AJAX to the route.
 */
  
+=======
+
+>>>>>>> 5e55c28a6ff318d0117f6e3ee7f165e991b69afb
 
 $(document).ready(function(){
   $('.delete-article').on('click',function(e){
     $target = $(e.target);
     console.log($target.attr('data-id'));
   });
+<<<<<<< HEAD
 });
 //use jquery to make AJAX request
+=======
+});//client side javascript file
+//use jquery to make AJAX request
+>>>>>>> 5e55c28a6ff318d0117f6e3ee7f165e991b69afb
